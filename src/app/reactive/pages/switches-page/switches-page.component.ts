eba90b717dc39d807412abe29aa5b602486c2527
Danny Armijos <daniel2667@hotmail.es>
2024-04-16 17:37:34 +0200

    mi register

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches-page',
   templateUrl: './switches-page.component.html',
-  styles: ``
+  styles: ``,
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
+  private fb = inject(FormBuilder);
 
+  ngOnInit(): void {
+    this.myForm.reset(this.person);
+  }
+
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', [Validators.required]],
+    wantNotification: [true, [Validators.required]],
+    termsAndConditions: [false, [Validators.requiredTrue]],
+  });
+
+  public person = {
+    gender: 'F',
+    wantNotification: false,
+  };
+
+  isValidField(field: string) {
+    return this.myForm.controls[field].errors;
+  }
+
+  onSave(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+
+    this.person = newPerson;
+    console.log(this.person);
+    console.log(this.myForm.value);
+  }
 }
